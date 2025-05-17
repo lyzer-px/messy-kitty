e2062c00767da9578e0a8bde24201b690a89d797
lyzer-px <liam.harfouche@epitech.eu>
2025-05-17 21:23:48 +0200

    feat: window opens

--- main.py
+++ main.py
@@ -10,34 +10,40 @@ import pygame as pg
 import random
 import time
 
+class Colors:
+    def __init__(self):
+        self.cyan = (0, 255, 255)
 
-Cyan = (0, 255, 255)
-
-class event:
+class Event:
     def __init__(self):
         self.event_total = 5
         self.event_prob = 1 # In percentage
 
-class screen:
+class Screen:
     def __init__(self):
         self.width = 1920
         self.height = 1080
-        self.window = pg.display.set_mode((self.screen.width, self.screen.height))
-        background = pg.Rect(0, 0, self.width, self.height)
+        self.background = pg.Rect(0, 0, self.width, self.height)
+        self.window = pg.display.set_mode((self.width, self.height))
 
-class game:
+class Game:
     def __init__(self):
         self.framerate = 60
         self.screen_width = 1920
         self.screen_height = 1080
-        self.screen = screen()
+        self.screen = Screen()
+        self.colors = Colors()
+        self.runs = True
         pg.init()
     def run(self):
-        while True:
-            pg.draw.rect(self.screen.background, Cyan, screen.window)
-
-
+        while self.run:
+            pg.display.update()
+            pg.draw.rect(self.screen.window, self.colors.cyan, self.screen.background)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.run = False
 
 
 if __name__ == '__main__':
+    game = Game()
     game.run()
