2c616e5f2f9f1fe41eca5ff36e61d92102c99200
lyzer-px <liam.harfouche@epitech.eu>
2025-05-17 21:02:16 +0200

    feat: added screen and event clas

--- main.py
+++ main.py
@@ -7,17 +7,33 @@
 ##
 
 import pygame as pg
+import random
+import time
 
 class event:
     def __init__(self):
         self.event_total = 5
+        self.event_prob = 1 # In percentage
+
+class screen:
+    def __init__(self):
+        self.width = 1920
+        self.height = 1080
+        background = pg.Rect(0, 0, self.width, self.height)
 
 class game:
     def __init__(self):
-        self.event_prob = 1 # In percentage
-    def run(self):
+        self.framerate = 60
+        self.screen_width = 1920
+        self.screen_height = 1080
+        self.screen = screen()
         pg.init()
-        pg.display.set_mode((1920, 1080))
+        pg.display.set_mode((self.screen.width, self.screen.height))
+    def run(self):
+        while True:
+            pg.draw.rect(self.screen.background)
+
+
 
 
 if __name__ == '__main__':
